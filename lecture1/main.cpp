53a175ac3eaacd82309ab15a873a046e44316795
Roland Sobczak <rolandsobczak@gmail.com>
2025-03-16 19:28:46 +0100

    refactor: method moved to struct

--- lecture1/main.cpp
+++ lecture1/main.cpp
@@ -8,19 +8,18 @@ struct Student {
   std::string name;
   std::string surname;
   std::vector<float> grades;
-};
 
-float calculate_grade(const Student &student) {
-  float sum =
-      std::accumulate(student.grades.begin(), student.grades.end(), 0.0f);
-  return sum / student.grades.size();
-}
+  float calculate_grade() {
+    float sum = std::accumulate(grades.begin(), grades.end(), 0.0f);
+    return sum / grades.size();
+  }
+};
 
 int main() {
   Student s1{"John", "Smile", {2, 3, 4}};
   Student s2{"Roland", "Sobczak", {4, 5, 4}};
 
-  cout << "grade = " << calculate_grade(s1) << endl;
+  cout << "grade = " << s1.calculate_grade() << endl;
 
   return 0;
 }
